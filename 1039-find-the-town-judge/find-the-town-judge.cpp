08b852614462275c07f8c51fd76b62256157a028
Sarthak Kashmira <103532185+SarthakKashmira@users.noreply.github.com>
2024-07-18 13:44:48 +0530

    Time: 155 ms (17.07%) | Memory: 73.7 MB (17.22%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> adj[n+1];
-        vector<int> adj1[n+1];
-        int ans=-1;
+        // vector<int> adj[n+1];
+        // vector<int> adj1[n+1];
+        // int ans=-1;
+        // for(auto i: trust)
+        // {
+        //     adj[i[0]].push_back(i[1]);
+        //     adj1[i[1]].push_back(i[0]);
+        // }
+        // for(int i=1;i<n+1;i++)
+        // {
+        //     if(adj[i].size()==0 && adj1[i].size()==n-1)
+        //     {
+        //         ans=i;
+        //     } 
+        // }
+        // return ans;
+        vector<vector<int>> v(n+1,vector<int>(2,0));
         for(auto i: trust)
         {
-            adj[i[0]].push_back(i[1]);
-            adj1[i[1]].push_back(i[0]);
+            v[i[1]][0]++;
+            v[i[0]][1]++;
         }
         for(int i=1;i<n+1;i++)
         {
-            if(adj[i].size()==0 && adj1[i].size()==n-1)
-            {
-                ans=i;
-            } 
+            if(v[i][0]==n-1 && v[i][1]==0)
+            {return i;}
         }
-        return ans;
+        return -1;
 
+        
     }
 };
