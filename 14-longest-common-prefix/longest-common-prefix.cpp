3f64c3a15f9119e7a3c625f3cf02324aa57e14d7
Sarthak Kashmira <103532185+SarthakKashmira@users.noreply.github.com>
2024-08-16 00:20:17 +0530

    Time: 3 ms (70.17%) | Memory: 11.1 MB (59.43%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,75 +1,25 @@
 class Solution {
 public:
-    class TrieNode{
-        public:
-        char data;
-        TrieNode* links[26];
-        bool flag;
-        int count;
-        TrieNode(char ch){
-            data=ch;
-            flag=false;
-            count=0;
-            for(int i=0;i<26;i++)
-            {links[i]=NULL;}
-        }
-
-    };
-    class Trie{
-        public:
-        TrieNode* root;
-        Trie(char ch)
-        {
-            root=new TrieNode(ch);
-        }
-        void insert(string word)
-        {
-            if(word.length()==0)
-            {
-                root->flag=true;
-                return ;
-            }
-            TrieNode* node=root;
-            for(auto c:word)
-            {
-                if(node->links[c-'a']==NULL)
-                {
-                    node->links[c-'a']=new TrieNode(c);
-                    node->count++;
-                }
-                    node=node->links[c-'a'];
-            }
-            node->flag=true;
-        }
-        void lcp(string first,string &ans)
-        {
-            TrieNode* node=root;
-            for(auto c:first)
-            {
-                if(node->count==1)
-                {
-                    ans.push_back(c);
-                    node=node->links[c-'a'];
-                }
-                else{
-                    break;
-                }
-                if(node->flag)
-                {break;}
-            }
-            return ;
-        }
-    };
     string longestCommonPrefix(vector<string>& strs) {
-        Trie* t=new Trie('\0');
-        for(auto i:strs)
+        if(strs.size()==1){return strs[0];}
+        int ans=1e9;
+        string temp=strs[0];
+        for(int i=1;i<strs.size();i++)
         {
-            if(i.size()==0) return "";
-            t->insert(i);
+            int k=0;
+            int j=0;
+            int count=0;
+            while(k<temp.size() && j<strs[i].size())
+            {
+                if(temp[k]==strs[i][j])
+                {count++;}
+                else{break;}
+                k++;
+                j++;
+            }
+            if(count<ans)
+            {temp=temp.substr(0,count);}
         }
-        string first=strs[0];
-        string ans="";
-        t->lcp(first,ans);
-        return ans;
+        return temp;
     }
 };
