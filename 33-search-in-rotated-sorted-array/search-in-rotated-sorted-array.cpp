28467a465f53ba854fc131638b8371a906c01d86
Sarthak Kashmira <103532185+SarthakKashmira@users.noreply.github.com>
2024-08-17 15:47:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (47.95%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-int binarysearch(vector<int>& nums, int target,int s,int e) {
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int s=0;
+        int e=n-1;
         while(s<=e)
         {
             int mid=s+(e-s)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>target)
+            if(nums[mid]==target)
+            {return mid;}
+            else if(nums[s]<=nums[mid] )
             {
-                e=mid-1;
+                if(nums[s]<=target && target<=nums[mid])
+                {e=mid-1;}
+                else{ s=mid+1;}
             }
-            else{
-                s=mid+1;
+            else if(nums[mid]<=nums[e]){
+                if(target>=nums[mid] && target<=nums[e])
+                {s=mid+1;}
+                else{
+                    e=mid-1;
+                }
             }
+
         }
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int s=0;
-        int e=nums.size()-1;
-        while(s<e)
-        {
-            int mid=s+(e-s)/2;
-            if(nums[mid]>=nums[0])
-            {s=mid+1;}
-            else{
-                e=mid;
-            }
-        }
-        
-            int ans1=binarysearch(nums, target,0,s-1);
-            int ans2=binarysearch(nums, target,s,nums.size()-1);
-
-            if(ans1!=-1){return ans1;}
-            else if(ans2!=-1){return ans2;}
-            return -1;
     }
 };
