82940eb3df2bed45afbd141fcd26d67b27db9fcf
Sarthak Kashmira <103532185+SarthakKashmira@users.noreply.github.com>
2024-10-25 11:41:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (38.93%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int st=0;
-        int end=n-1;
-     
-        while(st<=end)
-          {
-            int mid=(st+end)/2;
+        int low=0;
+        int high=matrix.size()-1;
+        while(low<high)
+        {
+            int mid=low+(high-low)/2;
             if(matrix[mid][0]==target)
-            {return true;}
+            {
+                return true;
+            }
+            else if(matrix[mid][0]<target && mid==(matrix.size()-1))
+            {
+                low=mid;
+                break;
+            }
+            else if(matrix[mid][0]<target && matrix[mid+1][0]>target)
+            {
+                low=mid;
+                break;
+            }
             else if(matrix[mid][0]>target)
             {
-                end=mid-1;
+                high=mid-1;
+            }else {
+                low=mid+1;
             }
-            else{
-                st=mid+1;
-            }
-          }
-        int row=end;
-        if(row<0) return false;
-        cout<<row;
-        st=0;
-        end=m-1;
-        while(st<=end)
+        }
+        cout<<low;
+        int n=low;
+        low=0;
+        high=matrix[n].size()-1;
+        if(high==0 && matrix[n][high]==target) return true;
+        while(low<=high)
         {
-            int mid=(st+end)/2;
-            if(matrix[row][mid]==target)
-            {return true;}
-            else if(matrix[row][mid]>target)
+            int mid=low+(high-low)/2;
+            if(matrix[n][mid]==target) return true;
+            else if(matrix[n][mid]>target){
+                high=mid-1;
+            }
+            else if(matrix[n][mid]<target)
             {
-                end=mid-1;
-            }else{
-                st=mid+1;
+                low=mid+1;
             }
         }
         return false;
